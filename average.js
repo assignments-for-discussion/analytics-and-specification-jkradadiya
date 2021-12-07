8199e71f0f265830685130e656937f13f6a91ed9
JAY RADADIYA <48830095+jkradadiya@users.noreply.github.com>
2021-12-07 09:58:08 +0530

    check

--- average.js
+++ average.js
@@ -1,6 +1,9 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  var numbers = numbers.filter(function(val) {
+    return !isNaN(val);
+  });
+  return numbers.reduce((p, c)=>   p + c, 0) / numbers.length;
 }
 
 module.exports = {average};
